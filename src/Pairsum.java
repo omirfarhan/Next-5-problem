bfeea0cde37c8fbf579ce811aa28a7956ecaaeb2
omirfarhan <162728245+omirfarhan@users.noreply.github.com>
2025-08-05 07:35:50 +0600

    git commit

--- src/Pairsum.java
+++ src/Pairsum.java
@@ -10,15 +10,39 @@ public class Pairsum {
     public static void main(String[] args){
 
         List<Integer> arrayList=new ArrayList<>(Arrays.asList(2,7,11,15));
-        int target=18;
+        int target=17;
 
         Pairsum ps=new Pairsum();
 
-        List<Integer> ans= ps.sumindex(arrayList,target);
+        List<Integer> ans= ps.pairsum(arrayList,target);
         System.out.println("Index number: "+ans);
 
     }
+    //optimal method
+    List<Integer> pairsum(List<Integer> list,int target){
 
+        List<Integer> arraylist=new ArrayList<>();
+
+        int i=0; int j=list.size()-1;
+
+        while(list.get(i) < list.get(j)){
+           int pairsums=list.get(i)+ list.get(j);
+
+           if (target<pairsums){
+               j--;
+           }else if(target>pairsums){
+               i++;
+           }else {
+               arraylist.add(i);
+               arraylist.add(j);
+               return arraylist;
+           }
+
+        }
+        return arraylist;
+    }
+
+    //brute force approach
     List<Integer> sumindex(List<Integer> arrayList,int target){
 
         List<Integer> ans=new ArrayList<>();
