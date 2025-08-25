08f191dd4fc21c8be69c9f5bf89c17a839fdd4c9
omirfarhan <162728245+omirfarhan@users.noreply.github.com>
2025-08-25 11:29:23 +0600

    git

---
+++ src/BruteOptimalMoore.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class BruteOptimalMoore {
+
+    public static void main(String[] args){
+
+       int[] arr={2,3};
+
+
+
+
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i-1]);
+        }
+
+    }
+}
